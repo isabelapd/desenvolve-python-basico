c111991a579ace0472df29eaa6246e12ef7ef93b
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 13:35:51 -0300

    Create questão2.py

---
+++ modulo2/aula 2/questão2.py
@@ -0,0 +1,10 @@
+text = "O resultado é"
+v1 = 10
+v2 = 3.5
+v3 = v1 + v2
+
+#variáveis na ordem de criação
+print (f"{text} {v1} {v2} {v3}")
+
+#tipos
+print (f" {type(text)} {type(v1)} {type(v2)} {type(v3)}")

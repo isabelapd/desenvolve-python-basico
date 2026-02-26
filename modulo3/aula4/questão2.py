ba862f1ced3759e818bd647608f61627531993f6
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 14:54:15 -0300

    Create questão2.py

---
+++ modulo3/aula4/questão2.py
@@ -0,0 +1,12 @@
+Avaliacao = int (input("Avalie o filme em uma escala de 1 a 5: "))
+
+if Avaliacao == 5:
+    print("Excelente!")
+elif Avaliacao == 4:
+    print("Muito Bom!")
+elif Avaliacao == 3:
+    print("Bom!")
+elif Avaliacao == 2:
+    print("Regular")
+else:
+    print("Ruim")    

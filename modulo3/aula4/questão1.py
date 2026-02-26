451b7274f7aa43d9133f48587e677b6411f495a6
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 14:50:12 -0300

    Create questão1.py

---
+++ modulo3/aula4/questão1.py
@@ -0,0 +1,6 @@
+n1 = int (input("Digite o valor 1: "))
+n2 = int (input("Digite o valor 2: "))
+if ((n1+n2) % 2) == 0:
+   print("A soma dos dois valores é par!")
+else:
+   print("A soma dos dois valores é impar!")   

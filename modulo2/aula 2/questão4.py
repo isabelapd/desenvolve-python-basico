9530cb73bbeb7aeef02f706761d7919a6fe7a321
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 13:42:40 -0300

    Create questão4.py

---
+++ modulo2/aula 2/questão4.py
@@ -0,0 +1,11 @@
+juros = 1.01
+saldo = 500.0
+print(saldo)
+saldo = saldo * juros # 505 rendimento1
+print (saldo)
+saldo = saldo * juros # 510 rendimento2
+print(saldo)
+saldo = saldo * juros # 515 rendimento3
+print(saldo)
+
+print (type(saldo))

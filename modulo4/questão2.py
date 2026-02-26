c775345d4b98feb09a5a372150e24de3732a5bfa
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 16:58:21 -0300

    Create questão2.py

---
+++ modulo4/questão2.py
@@ -0,0 +1,13 @@
+# Leitura do valor de n
+n = int(input("Digite um número: "))
+
+# Inicialização do contador
+cont = 0
+
+# Estrutura de repetição
+while cont < n:
+    cont += 1
+    print(cont)
+
+# Impressão final
+print("Fim")

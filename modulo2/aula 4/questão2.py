bd8fc9d833550d9b43d569b324cc0798dd21b9c2
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 13:54:58 -0300

    Create questão2.py

---
+++ modulo2/aula 4/questão2.py
@@ -0,0 +1,9 @@
+# Leiura de dados (entrada)
+fahrenheit= int(input("Digite a temperatura em Fahrenheit: "))
+
+# Processamento
+celsius= (fahrenheit 87- 32) * 5 / 9
+
+#Impressão de dados (saída)
+
+print (f"{fahrenheit} graus Fahrenheit são {int(celsius)} graus Celsius.")

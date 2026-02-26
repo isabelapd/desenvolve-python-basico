acb0968d7dfd6e8eba8f81dde54c2afb0bcfd14c
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-02-26 16:57:28 -0300

    Create questão1.py

---
+++ modulo4/questão1.py
@@ -0,0 +1,9 @@
+# Solicita um número ao usuário
+x = float(input("Digite um número: "))
+
+# Verifica se x é maior que 5
+if x > 5:
+    print("Maior que 5")   
+    
+# Sempre imprime "Fim"
+print("Fim")

4cac33cb496c076eab9e8f68d578598eb13831d1
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-03-05 16:13:50 -0300

    Update aula1_questão2.py

--- modulo5/aula1_questão2.py
+++ modulo5/aula1_questão2.py
@@ -1,1 +1,8 @@
+import random
+import math
 
+n = int(input("Digite a quantidade de números aleatórios entre 0 e 100: "))
+numeros = [random.randint(0, 100) for _ in range(n)]
+raiz_quadrada = math.sqrt(sum(numeros))
+
+print(f"Raiz quadrada da soma: {round(raiz_quadrada, 2)}")

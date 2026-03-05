be7a868f83595456939b5863a60b210c4dc07459
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-03-05 16:22:02 -0300

    Update aula1_questão5.py

--- modulo5/aula1_questão5.py
+++ modulo5/aula1_questão5.py
@@ -1,1 +1,11 @@
+import emoji
 
+print("Emojis disponíveis:")
+print(" ❤️- :red_heart: ")
+print(" 👍- :thumbs_up: ")
+print(" 🤔- :thinking_face: ")
+print(" 🥳- :partying_face: ")
+
+frase_codificada = input("Digite uma frase e ela será emojizada: ")
+frase_emojizada = emoji.emojize(frase_codificada, language='alias')
+print(f"Frase emojizada: {frase_emojizada}")

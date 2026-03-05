eac357337f02aa92549e144b2174f9433f135528
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-03-05 16:15:32 -0300

    Update aula1_questão3.py

--- modulo5/aula1_questão3.py
+++ modulo5/aula1_questão3.py
@@ -1,1 +1,12 @@
+import random
 
+numero_secreto = random.randint(1,10)
+while True:
+    palpite = int(input("Adivinhe o número entre 1 e 10: "))
+    if palpite < numero_secreto:
+        print("Muito baixo, tente novamente!")
+    elif palpite > numero_secreto:
+        print("Muito alto, tente novamente!")
+    else:
+        print(f"Correto! O número é {numero_secreto}.")
+        break

70c7edfda159c90bd1db1e7489a694f61d50dc78
isabelapd <isabelajabour.fernandespdita908@pditabira.com>
2026-03-05 16:16:29 -0300

    Update aula1_questão4.py

--- modulo5/aula1_questão4.py
+++ modulo5/aula1_questão4.py
@@ -1,1 +1,7 @@
+import datetime
 
+agora = datetime.datetime.now()
+data_formatada = agora.strftime("%d/%m/%Y")
+hora_formatada = agora.strftime("%H:%M")
+print(f"Data: {data_formatada}")
+print(f"Hora: {hora_formatada}")
